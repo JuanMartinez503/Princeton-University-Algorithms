855e37523a6494c7cb326bfd45b53dd7c6028611
Juan Martinez <juanmartinez5678@hotmail.com>
2024-05-09 22:40:12 -0400

    added the rest of the notes for union-finds

--- part-1/week-1/union-finds.java
+++ part-1/week-1/union-finds.java
@@ -68,3 +68,41 @@
      * uf.union(p, q);
      * StdOut.println(p + " " + q);}
      */
+
+
+// Quick-find
+// Data structure
+// Integer array id[] of size N
+// Interpretation:  p and q are connected if they have the same id
+// Find:  check if p and q have the same id
+// Union:  to merge components containing p and q, change all entries whose id equals id[p] to id[q]
+
+// Quick-find defect
+
+//quick find java implementation
+public class QuickFindUF{
+    private int[] id;
+
+    public QuickFindUF(int N){
+        id = new int[N];
+        for(int i = 0; i < N; i++){
+            id[i] = i;
+        }
+    }
+
+    public boolean connected(int p, int q){
+        return id[p] == id[q];
+    }
+
+    public void union(int p, int q){
+        int pid = id[p];
+        int qid = id[q];
+        for(int i = 0; i < id.length; i++){
+            if(id[i] == pid){
+                id[i] = qid;
+            }
+        }
+    }
+}
+// quick find is too slow
+// union is too expensive
